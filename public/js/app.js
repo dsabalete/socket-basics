caca50cf51422079b364bbbbdeaf26dfa05220e1
David Sabalete <dsabalete@gmail.com>
2016-09-06 11:59:32 +0200

    Show timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,11 @@ socket.on('connect', function () {
 });
 
 socket.on('message', function (message) {
+    var momentTimestamp = moment.utc(message.timestamp);
     console.log('New message:');
     console.log(message.text);
 
-    jQuery('.messages').append('<p>' + message.text + '</p>');
+    jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong> ' + message.text + '</p>');
 });
 
 // Handles submitting of new message
@@ -20,7 +21,8 @@ $form.on('submit', function (event) {
     var $message = $form.find('input[name=message]');
 
     socket.emit('message', {
-        text: $message.val()
+        text: $message.val(),
+        timestamp: moment()
     });
 
     $message.val('');
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,12 +14,16 @@ io.on('connection', function (socket) {
     socket.on('message', function (message) {
         console.log('Message received: ' + message.text);
         
-        //socket.broadcast.emit('message', message);
+        message.timestamp = moment().valueOf();
+
         io.emit('message', message);
     });
 
+    // timestamp property - JavaScript timestamp (milliseconds)
+
     socket.emit('message', {
-        text: 'Welcome to the chat application!'
+        text: 'Welcome to the chat application!',
+        timestamp: moment().valueOf()
     });
 
 });
